eb576ecdf90d574c9832d3f1f0d33cdd1aac3f47
Volk <j_knz12@hotmail.com>
2025-10-07 00:15:33 +1000

    installed socket.io.client + barebone socket.ts

--- TextVideoChatSystem/src/app/services/socket.ts
+++ TextVideoChatSystem/src/app/services/socket.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { io } from 'socket.io-client';
+
+const SERVER_URL = "http://localhost:3000"
+
+interface ChatMessage{
+  userID: string,
+  content: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class Socket {
+  private socket: any;
+  messages = signal<ChatMessage[]>([]);
+  users = signal<string[]>([]);
+  currentChannel = signal<string | null>(null);
+  constructor () {}
+
+  initSocket(){
+    this.socket = io(SERVER_URL);
+    return ()=>{this.socket.disconnect();}
+  }
   
 }
