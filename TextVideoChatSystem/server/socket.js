25a6bf7a162ed8fc66da16f38a796744458438c6
Volk <j_knz12@hotmail.com>
2025-10-07 00:05:36 +1000

    Added uhh back-end sockets
    I also created service socket in front-end but there's nothing there yet

--- TextVideoChatSystem/server/socket.js
+++ TextVideoChatSystem/server/socket.js
@@ -2,9 +2,40 @@ function connect(io, port){
     io.on('connection', (socket) => {
         console.log(`User connected on port ${port}: ${socket.id}`);
 
+        // send message
         socket.on('message', (message) => {
             console.log(`message: ${message}`);
             io.emit("message", message)
+        })
+
+        // send image
+        socket.on('image', (image) => {
+            console.log(`image: ${image}`);
+            io.emit("image", image)
+        })
+
+        // join channel
+        socket.on('joinChannel', ({channelID, userID}) => {
+            socket.join(channelID)
+            io.to(channelID).emit("userJoin", userID)
+        })
+
+        // leave channel
+        socket.on('leaveChannel', ({channelID, userID}) => {
+            socket.leave(channelID)
+            io.to(channelID).emit("userJoin", userID)
+        })
+        
+        // join group
+        socket.on('joinGroup', ({groupID, userID}) => {
+            socket.join(groupID)
+            io.to(groupID).emit("userJoin", userID)
+        })
+
+        // leave group
+        socket.on('leaveGroup', ({groupID, userID}) => {
+            socket.leave(groupID)
+            io.to(groupID).emit("userJoin", userID)
         })
     })
 }
