12440c1aa312ea75f065bc655c8380dd5bea01c7
Volk <j_knz12@hotmail.com>
2025-10-10 13:53:44 +1000

    Added new field(s) for group collection and users

--- TextVideoChatSystem/server/initializeMongo/addGroups.js
+++ TextVideoChatSystem/server/initializeMongo/addGroups.js
@@ -1,95 +1,97 @@
-async function addGroups(collection){
+async function addGroups(collection) {
     await collection.drop().catch(() => {});
 
-    await collection.insertMany(
-        [
-            {
-                "groupID": "g1",
-                "groupName": "TestGroup",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "g2",
-                "groupName": "FunGroup",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "g3",
-                "groupName": "ProjectGroup",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "g4",
-                "groupName": "HobbyGroup",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "g5",
-                "groupName": "StudyGroup",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep05_1007132",
-                "groupName": "Burrito",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep05_1626400",
-                "groupName": "test123",
-                "bannedUsers": [
-                {
-                    "userID": "6",
-                    "reason": ""
-                },
-                {
-                    "userID": "3",
-                    "reason": "test"
-                }
-                ]
-            },
-            {
-                "groupID": "gSep05_16302713",
-                "groupName": "etet",
-                "bannedUsers": [
-                {
-                    "userID": "3",
-                    "reason": "stinky"
-                }
-                ]
-            },
-            {
-                "groupID": "gSep05_1846285",
-                "groupName": "asasdadada",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep05_2357059",
-                "groupName": "PLEASE WORK",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep06_21101417",
-                "groupName": "TESTTEST",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep06_2113193",
-                "groupName": "bruh",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep11_09093619",
-                "groupName": "TestGroup",
-                "bannedUsers": []
-            },
-            {
-                "groupID": "gSep11_0910496",
-                "groupName": "NewGroup",
-                "bannedUsers": []
-            }
-        ]
-    )
+    await collection.insertMany([
+        {
+            "groupID": "g1",
+            "groupName": "TestGroup",
+            "bannedUsers": [],
+            "serverPic": "images/server/serverPic/unknown.png" 
+        },
+        {
+            "groupID": "g2",
+            "groupName": "FunGroup",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "g3",
+            "groupName": "ProjectGroup",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "g4",
+            "groupName": "HobbyGroup",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "g5",
+            "groupName": "StudyGroup",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep05_1007132",
+            "groupName": "Burrito",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep05_1626400",
+            "groupName": "test123",
+            "bannedUsers": [
+                { "userID": "6", "reason": "" },
+                { "userID": "3", "reason": "test" }
+            ],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep05_16302713",
+            "groupName": "etet",
+            "bannedUsers": [
+                { "userID": "3", "reason": "stinky" }
+            ],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep05_1846285",
+            "groupName": "asasdadada",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep05_2357059",
+            "groupName": "PLEASE WORK",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep06_21101417",
+            "groupName": "TESTTEST",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep06_2113193",
+            "groupName": "bruh",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep11_09093619",
+            "groupName": "TestGroup",
+            "bannedUsers": [],
+            "serverPic": ""
+        },
+        {
+            "groupID": "gSep11_0910496",
+            "groupName": "NewGroup",
+            "bannedUsers": [],
+            "serverPic": ""
+        }
+    ]);
 }
 
-module.exports = {addGroups}
-
+module.exports = { addGroups };
--- TextVideoChatSystem/server/initializeMongo/addUsers.js
+++ TextVideoChatSystem/server/initializeMongo/addUsers.js
@@ -1,95 +1,85 @@
-async function addUsers(collection){
+async function addUsers(collection) {
     await collection.drop().catch(() => {});
 
-    await collection.insertMany(
-        [
-            {
-                "id": "1",
-                "email": "og@email.com",
-                "username": "super",
-                "pass": "123",
-                "avatar": "images/pfp/brownie.png",
-                "roles": [
-                "chatUser",
-                "superAdmin"
-                ],
-                "signedIn": false
-            },
-            {
-                "id": "2",
-                "email": "user2@email.com",
-                "username": "userTwo",
-                "pass": "123",
-                "avatar": "images/pfp/defaultPFP.jpg",
-                "roles": [
-                "chatUser",
-                "groupAdmin"
-                ],
-                "signedIn": false
-            },
-            {
-                "id": "3",
-                "email": "user3@email.com",
-                "username": "userThree",
-                "pass": "123",
-                "avatar": "images/pfp/defaultPFP.jpg",
-                "roles": [
-                "chatUser",
-                "groupAdmin"
-                ],
-                "signedIn": false
-            },
-            {
-                "id": "5",
-                "email": "user5@email.com",
-                "username": "userFive",
-                "pass": "123",
-                "avatar": "images/pfp/defaultPFP.jpg",
-                "roles": [
-                "chatUser",
-                "groupAdmin"
-                ],
-                "signedIn": false
-            },
-            {
-                "id": "6",
-                "email": "user6@email.com",
-                "username": "userSix",
-                "pass": "123",
-                "avatar": "images/pfp/defaultPFP.jpg",
-                "roles": [
-                "chatUser"
-                ],
-                "signedIn": false
-            },
-            {
-                "id": "7",
-                "email": "user7@email.com",
-                "username": "userSeven",
-                "pass": "123",
-                "avatar": "images/pfp/defaultPFP.jpg",
-                "roles": [
-                "chatUser",
-                "groupAdmin",
-                "superAdmin"
-                ],
-                "signedIn": false
-            },
-            {
-                "id": "Sep06_1903296",
-                "email": "gooner@email.com",
-                "username": "bruh",
-                "pass": "123",
-                "avatar": "images/pfp/defaultPFP.jpg",
-                "roles": [
-                "chatUser",
-                "groupAdmin",
-                "superAdmin"
-                ],
-                "signedIn": false
-            }
-        ]
-    )
+    await collection.insertMany([
+        {
+            "id": "1",
+            "email": "og@email.com",
+            "username": "super",
+            "pass": "123",
+            "avatar": "images/pfp/brownie.png",
+            "roles": ["chatUser", "superAdmin"],
+            "signedIn": false,
+            "statusMessage": "Hey, I am using ChatApp!",
+            "dateJoined": new Date().toISOString()
+        },
+        {
+            "id": "2",
+            "email": "user2@email.com",
+            "username": "userTwo",
+            "pass": "123",
+            "avatar": "images/pfp/defaultPFP.jpg",
+            "roles": ["chatUser", "groupAdmin"],
+            "signedIn": false,
+            "statusMessage": "Available",
+            "dateJoined": new Date().toISOString()
+        },
+        {
+            "id": "3",
+            "email": "user3@email.com",
+            "username": "userThree",
+            "pass": "123",
+            "avatar": "images/pfp/defaultPFP.jpg",
+            "roles": ["chatUser", "groupAdmin"],
+            "signedIn": false,
+            "statusMessage": "Busy",
+            "dateJoined": new Date().toISOString()
+        },
+        {
+            "id": "5",
+            "email": "user5@email.com",
+            "username": "userFive",
+            "pass": "123",
+            "avatar": "images/pfp/defaultPFP.jpg",
+            "roles": ["chatUser", "groupAdmin"],
+            "signedIn": false,
+            "statusMessage": "At work",
+            "dateJoined": new Date().toISOString()
+        },
+        {
+            "id": "6",
+            "email": "user6@email.com",
+            "username": "userSix",
+            "pass": "123",
+            "avatar": "images/pfp/defaultPFP.jpg",
+            "roles": ["chatUser"],
+            "signedIn": false,
+            "statusMessage": "Available",
+            "dateJoined": new Date().toISOString()
+        },
+        {
+            "id": "7",
+            "email": "user7@email.com",
+            "username": "userSeven",
+            "pass": "123",
+            "avatar": "images/pfp/defaultPFP.jpg",
+            "roles": ["chatUser", "groupAdmin", "superAdmin"],
+            "signedIn": false,
+            "statusMessage": "Online",
+            "dateJoined": new Date().toISOString()
+        },
+        {
+            "id": "Sep06_1903296",
+            "email": "gooner@email.com",
+            "username": "bruh",
+            "pass": "123",
+            "avatar": "images/pfp/defaultPFP.jpg",
+            "roles": ["chatUser", "groupAdmin", "superAdmin"],
+            "signedIn": false,
+            "statusMessage": "Chillin'",
+            "dateJoined": new Date().toISOString()
+        }
+    ]);
 }
 
-module.exports = {addUsers}
+module.exports = { addUsers };
